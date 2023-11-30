d3cb49b5786a7184f31cd1adf994c4a8bd2eb154
origin1508 <origin1508@gmail.com>
2023-11-30 18:49:42 +0900

    feat: Register Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
-
+import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+import Register from "../components/Register";
+
+const RegisterPage = () => {
+  return (
+    <Layout>
+      <Register />
+    </Layout>
+  );
+};
+
+export default RegisterPage;
