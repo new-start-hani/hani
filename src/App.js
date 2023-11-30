233dfd9c7c87d2ed9cb1534fe393691766cae060
seunghr <bczx1208@naver.com>
2023-12-01 03:27:52 +0900

    mypage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from "./components/Login";
 import RegisterPage from "./pages/RegisterPage";
 import FeedPage from "./pages/FeedPage";
 import Detail from "./components/detail";
+import Mypage from "./components/Mypage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/feed" element={<FeedPage />} />
           <Route path="/detail" element={<Detail />} />
+          <Route path="/mypage" element={<Mypage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Mypage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Mypage() {
+  return (
+    <div className="w-[1000px] bg-[#dedede] py-[100px] rounded-b-lg mx-auto">
+      <div className="w-96 h-96 bg-black rounded-full mx-auto"></div>
+      <div className="w-[1000px] text-center mt-[20px]">
+        <div className="font-bold text-[30px]">name</div>
+        <div className="">12.31일은 즐거운날!</div>
+      </div>
+    </div>
+  );
+}
+
+export default Mypage;
